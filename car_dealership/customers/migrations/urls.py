e9403fb3cf1376b7e6b6805632bf192012c7fe13
emr-tech <62887880+elenimorava@users.noreply.github.com>
2024-02-22 16:57:21 +0100

    //

--- car_dealership/customers/migrations/urls.py
+++ car_dealership/customers/migrations/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     path('customers/', views.CustomerListCreate.as_view(), name='customer-list-create'),
     path('customers/<int:pk>/', views.CustomerRetrieveUpdateDestroy.as_view(), name='customer-retrieve-update-destroy'),
 ]
+
